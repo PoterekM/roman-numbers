ab34f8e2fccf712e350298c84a82a2e84683f678
Michelle Poterek and Max Scher <michelle@>
2017-06-08 11:46:51 -0700

    display numerals for thousands value

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
 //BizNasty Logic Below
+var romanNumNums = function(userInput) {
+  var numeralArr = ["I", "V", "X", "L", "C", "D", "M"];
+  var resultArr = [];
 
+  if (userInput / 1000 >= 1) {
+    var thouMult = userInput / 1000;
+    var thouSlot = numeralArr[6].repeat(thouMult);
+    resultArr.push(thouSlot);
+  }
 
+  var resultString = resultArr.toString();
+  return resultString;
 
-
+};
 //UI Logic Below
 $(document).ready(function() {
   $("#submit").submit(function(event) {
