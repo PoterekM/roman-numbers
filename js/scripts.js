85934a771cd0039b0270a2fc31220891399ef140
Michelle Poterek and Max Scher <max@>
2017-06-08 11:18:25 -0700

    add UI logic in JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
+//BizNasty Logic Below
+
+
+
+
+//UI Logic Below
 $(document).ready(function() {
-
-
-
-})
+  $("#submit").submit(function(event) {
+    event.preventDefault();
+    var userInput = parseInt($("input#num-enter").val());
+    var finalResult = romanNumNums(userInput);
+    $("finalResult").text(finalResult);
+  });
+});
